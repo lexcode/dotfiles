254681ca70d970de6dd3c0f9b47dabef137742e1
Alex Oliveira <349424+lexcode@users.noreply.github.com>
2021-06-30 17:38:33 +0100

    List updated

--- .scripts/mac.sh
+++ .scripts/mac.sh
@@ -5,6 +5,18 @@ brew install --cask dash
 brew install --cask dcommander
 brew install --cask figma
 brew install --cask spotify
+brew install --cask firefox
+brew install --cask google-chrome
+brew install --cask alfred
+brew install --cask spectacle
+brew install --cask 1password
+brew install --cask whatsapp
+brew install --cask brave-browser
+brew install --cask discord
+brew install --cask docker
+
+brew install neovim
 brew install htop
 brew install tmux
 brew install yarn
+
